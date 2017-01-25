f6a1ea0335e17b084415f654da059be762adf9c8
Bob Weinand <bobwei9@hotmail.com>
2017-01-25 12:42:27 +0100

    Make parser more readable / drop gotos

--- lib/functions.php
+++ lib/functions.php
@@ -9,62 +9,31 @@ function generateId(int $length = 32): string
 
 function parse(): \Generator
 {
-    start: {
-        $data = "";
-        $command = null;
-        $headers = null;
-        $body = null;
-        $contentLength = null;
-    }
+    $data = yield;
 
-    recv_command: {
-        $data .= yield;
-        goto parse_command;
-    }
-
-    parse_command: {
+    while (true) {
         $data = \ltrim($data, "\r\n");
-        $eolPosition = \strpos($data, "\n");
-        if ($eolPosition === false) {
+        while ($data == "") {
+            $data = \ltrim(yield, "\r\n");
+        }
+
+        while (($eolPosition = \strpos($data, "\n")) === false) {
             // @TODO prevent DoS (for servers) by enforcing
             // optional max header buffer size
-            goto recv_command;
+            $data .= yield;
         }
+
         $command = \rtrim(\substr($data, 0, $eolPosition), "\r\n");
         $data = \substr($data, $eolPosition);
 
-        // skip recv_headers to avoid pausing the parser until necessary
-        goto standard_headers;
-    }
-
-    recv_headers: {
-        $data .= yield;
-        goto standard_headers;
-    }
-
-    standard_headers: {
-        $headerEndPosition = \strpos($data, "\n\n");
-        if ($headerEndPosition === false) {
-            goto windows_headers;
+        while (($rawHeaders = \strstr($data, "\n\n", true)) === false && ($rawHeaders = \strstr($data, "\r\n\r\n", true)) === false) {
+            $data .= yield;
         }
-        $rawHeaders = \trim(\substr($data, 0, $headerEndPosition));
-        $data = \substr($data, $headerEndPosition + 2);
-        goto parse_headers;
-    }
 
-    windows_headers: {
-        $headerEndPosition = \strpos($data, "\r\n\r\n");
-        if ($headerEndPosition === false) {
-            goto recv_headers;
-        }
-        $rawHeaders = \trim(\substr($data, 0, $headerEndPosition));
-        $data = \substr($data, $headerEndPosition + 4);
-        goto parse_headers;
-    }
+        $data = \substr($data, \strlen($rawHeaders) + ($data[\strlen($rawHeaders)] === "\n" ? 2 : 4));
 
-    parse_headers: {
         $headers = [];
-        $headerLines = \preg_split("/[\r?\n]+/", $rawHeaders);
+        $headerLines = \preg_split("/[\r?\n]+/", $rawHeaders, -1, PREG_SPLIT_NO_EMPTY);
         foreach ($headerLines as $headerLine) {
             $headerDetails = \explode(':', $headerLine, 2);
             $field = $headerDetails[0];
@@ -77,62 +46,20 @@ function parse(): \Generator
 
         if (isset($headers["content-length"])) {
             $contentLength = (int) $headers["content-length"];
-            goto parse_body_by_length;
+            while (!isset($data[$contentLength])) {
+                $data .= yield;
+            }
+            // Note that even with content-length the spec requires messages to terminate with a null-byte
         } else {
-            goto parse_body_to_null_byte;
-        }
-    }
-
-    recv_body: {
-        $data .= yield;
-
-        // @TODO prevent DoS (for servers) by enforcing
-        // optional max body buffer size
-
-        if (isset($contentLength)) {
-            goto parse_body_by_length;
-        } else {
-            goto parse_body_to_null_byte;
-        }
-    }
-
-    parse_body_by_length: {
-        if (!isset($data[$contentLength])) {
-            goto recv_body;
+            while (($contentLength = strpos($data, "\0")) === false) {
+                $data .= yield;
+            }
         }
 
         $body = \substr($data, 0, $contentLength);
-        // Add to the offset because even with content-length
-        // the spec requires messages to terminate with a null-byte
         $data = \substr($data, $contentLength + 1);
 
-        goto notify;
-    }
-
-    parse_body_to_null_byte: {
-        if (($nullBytePosition = \strpos($data, "\0")) === false) {
-            goto recv_body;
-        }
-
-        $body = \substr($data, 0, $nullBytePosition);
-        $data = \substr($data, $nullBytePosition + 1);
-
-        goto notify;
-    }
-
-    notify: {
         $frame = new Frame($command, $headers, $body);
-        $command = null;
-        $headers = null;
-        $body = null;
-        $contentLength = null;
-
         $data .= yield $frame;
-
-        if (isset($data[0])) {
-            goto parse_command;
-        } else {
-            goto recv_command;
-        }
     }
 }
